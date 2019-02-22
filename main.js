0537c16cddcba4c68be2fb6efb479c96de2bdb23
Anastasia Repina <anastasiya.repina2012@yandex.ru>
2019-02-22 12:40:55 +0300

    task 2 done

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+var cache = new Object();
+const p1 = getJSON("theaters.json");
+console.log(getData('getData'));
+console.log(getData('getData'));
+
+Promise.all([p1]).then(values => {
+    for (var i = 0; i < values[0].length; i++) {
+        var value = values[0][i];
+        //console.log(value);
+        var table = document.getElementById("dataTable");
+        var rowCount = table.rows.length;
+        var row = table.insertRow(rowCount);
+        var cell1 = row.insertCell(0);
+        cell1.innerHTML = value["WebSite"];
+
+        var cell2 = row.insertCell(1);
+        cell2.innerHTML = value["FullName"];
+    }
+    console.log("done")
+}, function(reason) {
+    console.log("reason", reason);
+});
+
 function getJSON(url) {
     return new Promise(function (resolve, reject) {
         const request = new XMLHttpRequest();
@@ -20,28 +43,13 @@ function getJSON(url) {
     });
 }
 
-function getData() {
-    return new Promise(resolve => {
-        setTimeout(() => resolve(42), 1000)
-    })
-}
-
-const p1 = getJSON("theaters.json");
-
-Promise.all([p1]).then(values => {
-    for (var i = 0; i < values[0].length; i++) {
-        var value = values[0][i];
-        console.log(value);
-        var table = document.getElementById("dataTable");
-        var rowCount = table.rows.length;
-        var row = table.insertRow(rowCount);
-        var cell1 = row.insertCell(0);
-        cell1.innerHTML = value["WebSite"];
-
-        var cell2 = row.insertCell(1);
-        cell2.innerHTML = value["FullName"];
+function getData(name) {
+    if(!cache[name]) { //no data in cache
+        cache[name] = new Promise(resolve => {
+            setTimeout(() => resolve(42), 1000)
+        }); // save data in cache
+        return cache[name]
+    }else{//already have data in cache
+        return cache[name]
     }
-    console.log("done")
-}, function(reason) {
-    console.log("reason", reason);
-});
+}
